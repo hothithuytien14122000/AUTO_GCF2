23aad9c4e6b39ae58e51818d9c8baa9648e5a56b
ThuyTien <you@example.com>
2023-05-05 11:34:06 +0700

    pots

--- cypress/e2e/gcf2.feature
+++ cypress/e2e/gcf2.feature
@@ -4,16 +4,58 @@ Feature: GCF2
         When User enter email geneat.soft@gmail.com
         When User enter Password 123456
         When User click submit Login
-    Scenario: SI Add Long form
+    Scenario: LF-01 Verify that Add New successfuly with enter data
         Given User opens admin Gcf2
         When User enter email geneat.soft@gmail.com
         When User enter Password 123456
         When User click submit Login
-        When User click Long form
-        When User click Add new
-        # And User select Chuyên mục
-        When User enter data file Tiêu đề, Tác giả, Giới thiệu, Nội dung
-        When User switch tab English and data in fields Tiêu đề, Tác giả, Giới thiệu, Nội dung
-        When User Add Ảnh thumbanil
-        When  User Enter data in fields Background Color, Custom CSS Class, Custom CSS
-        When User Click button Lưu lại
+        When User Click the button Add new
+        When User Select Category
+        When User Enter data in fields Title Nông dân và Lớp học đồng ruộng
+        When User Enter data in fields Author Nguyễn Thị Phương Dung
+        When User Enter data in fields Introduce Chị Hầm là một trong số 1150 nông dân trong 9 xã tham gia Lớp học đồng ruộng (FFS) về chủ đề 
+        When User Enter data in fields Content Trước giờ tôi không biết đất cũng có vị chua hay mặn và nó ảnh hưởng lớn tới cây trồng
+        When User Switch the tab to English
+        When User Enter English data in the fields Title Binh Thuan farmers and Farmer
+        When User Enter English data in fields Author Nguyen Thi Phuong Dung
+        When User Enter English data in fields Introduce Mrs. Ham is among the 1150 farmers from 9 communes taking part in the FFS on the topic of Land and Biomass Management
+        When User Enter English data in fields Content Mrs. Ham is among the 1150 farmers from 9 communes taking part in the FFS on the topic of 
+        When User Add thumbanil
+        When User Enter data in fields Background Color #186a5e
+        When User Enter data in fields Custom CSS Class text-white
+        # When User Enter data in fields Custom CSS .Longform p { 
+        # line-height: 3rem !important; 
+        # font-weight: 900 !important;
+        # font-style: italic !important; 
+        # font-size: 1.5rem !important; 
+        # text-align: justify !important; 
+        # }
+        And User Click the button Save
+     Scenario: LF-02 Verify that  edit successfully
+        Given User opens admin Gcf2
+        When User enter email geneat.soft@gmail.com
+        When User enter Password 123456
+        When User click submit Login
+        When User Click the button Edit
+        When User edit Category
+        When User edit Title Nông sản Việt Nam
+        When User edit Author Nguyễn Thị Phương Dung
+        When User edit Introduce Chị Hầm là một trong số 1150 nông dân trong 9 xã tham gia Lớp học đồng ruộng (FFS) về chủ đề 
+        When User edit Content Trước giờ tôi không biết đất cũng có vị chua hay mặn và nó ảnh hưởng lớn tới cây trồng
+        When User Switch the tab to English
+         When User Enter English data in the fields Title Binh Thuan farmers and Farmer
+        When User Enter English data in fields Author Nguyen Thi Phuong Dung
+        When User Enter English data in fields Introduce Mrs. Ham is among the 1150 farmers from 9 communes taking part in the FFS on the topic of Land and Biomass Management
+        When User Enter English data in fields Content Mrs. Ham is among the 1150 farmers from 9 communes taking part in the FFS on the topic of 
+        When User Add thumbanil
+        When User Enter data in fields Background Color #186a5e
+        When User Enter data in fields Custom CSS Class text-white
+        # When User Enter data in fields Custom CSS .Longform p { 
+        # line-height: 3rem !important; 
+        # font-weight: 900 !important;
+        # font-style: italic !important; 
+        # font-size: 1.5rem !important; 
+        # text-align: justify !important; 
+        # }
+        And User Click the button Save
+    
--- cypress/support/step_definitions/steps.ts
+++ cypress/support/step_definitions/steps.ts
@@ -10,43 +10,89 @@ Given("User opens {} Gcf2", (menu) => {
     cy.get('#username').should('have.value', email);
   });
   When("User enter Password {}", (password) => {
-    cy.get('#password').type(password)
+    cy.get('#password').type(password);
     cy.get('#password').should('have.value', password);
   });
  When ("User click submit Login", () =>{
   cy.get('#button-submit').click();
  })
-When("User click Long form", ()=>{
-  cy.get('.relative > :nth-child(2) > :nth-child(2) > .truncate').click()
+When("User Click the button Add new", ()=>{
+  cy.get('#undefined').click();
  })
-When("User click Add new", ()=>{
-  cy.get('#undefined').click()
+When("User Select Category", ()=>{
+  cy.get('#categoryId > .ant-select-selector > .ant-select-selection-search').click();
+  cy.get('[title="Press release"] > .ant-select-item-option-content').click();
  })
-//  When("User select Chuyên mục", ()=>{
-//   cy.get('#categoryId > .ant-select-arrow').click();
-//  })
- When("User enter data file Tiêu đề, Tác giả, Giới thiệu, Nội dung", ()=>{
-  cy.get('#translations-vn-title').type('Nông sản');
-  cy.get('#translations-vn-author').type('Nguyen Van A');
-  cy.get('#translations-vn-summary').type('text1');
-  cy.get('.codex-editor__redactor').type('text2');
+ When("User Enter data in fields Title {}", (Title)=>{
+  cy.get('#translations-vn-title').click().clear().type(Title);
+  cy.get('#translations-vn-title').should('have.value', Title);
  })
- When("User switch tab English and data in fields Tiêu đề, Tác giả, Giới thiệu, Nội dung", ()=>{
+  When("User Enter data in fields Author {}", (Author)=>{
+  cy.get('#translations-vn-author').click().clear().type(Author);
+  cy.get('#translations-vn-author').should('have.value',Author);
+  })
+  When("User Enter data in fields Introduce {}", (Introduce)=>{
+  cy.get('#translations-vn-summary').click().clear().type(Introduce);
+  cy.get('#translations-vn-summary').should('have.value',Introduce);
+  })
+  When("User Enter data in fields Content {}", (Content)=>{
+  cy.get('.codex-editor__redactor').click().clear().type(Content);
+ })
+ When("User Switch the tab to English", ()=>{
   cy.get('.ant-tabs-nav-list > :nth-child(2)').click();
-  cy.get('#translations-en-title').type('Nong san');
-  cy.get('#translations-en-author').type('Nguyen Van A');
-  cy.get('#translations-en-summary').type('text1');
-  cy.get('.codex-editor__redactor').type('texxt2');
  })
- When ("User Add Ảnh thumbanil",()=>{
+ When("User Enter English data in the fields Title {}", (TitleEnglish)=>{
+  cy.get('#translations-en-title').click().clear().type(TitleEnglish);
+  cy.get('#translations-en-title').should('have.value', TitleEnglish);
+ })
+  When("User Enter English data in fields Author {}", (AuthorEnglish)=>{
+  cy.get('#translations-en-author').click().clear().type(AuthorEnglish);
+  cy.get('#translations-en-author').should('have.value',AuthorEnglish);
+  })
+  When("User Enter English data in fields Introduce{}", (IntroduceEnglish)=>{
+  cy.get('#translations-en-summary').click().clear().type(IntroduceEnglish);
+  cy.get('#translations-en-summary').should('have.value',IntroduceEnglish);
+  })
+  When("User Enter English data in fields Content {}", (ContentEnglish)=>{
+  cy.get('.codex-editor__redactor').click().clear().type(ContentEnglish);
+ })
+ When ("User Add thumbanil",()=>{
     cy.get(':nth-child(2) > .ng-untouched > .flex > .relative > #undefined').selectFile('cypress/fixtures/image/image.jpg', { action: 'drag-drop' })
 })
-When ("User Enter data in fields Background Color, Custom CSS Class, Custom CSS",()=>{
-  cy.get(':nth-child(1) > .ant-row > .ant-form-item-control > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-input').click().type('#8AC653');
-  cy.get('#customCSSClass').click().type('text-white');
-  cy.get('#customCSS').click().type('line-height: 3rem !important; font-weight: 900 !important;font-style: italic !important; font-size: 1.5rem !important;  text-align: justify !important;')
- })
-When ("User Click button Lưu lại",()=>{
+When ("User Enter data in fields Background Color {}",(BackgroundColor)=>{
+  cy.get(':nth-child(1) > .ant-row > .ant-form-item-control > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-input').click().clear().type(BackgroundColor);
+})
+When ("User Enter data in fields Custom CSS Class {}",(CustomCSSClass)=>{
+  cy.get('#customCSSClass').click().clear().type(CustomCSSClass);
+  cy.get('#customCSSClass').should('have.value',CustomCSSClass);
+})
+// When ("User Enter data in fields Custom CSS {}",(CustomCSS)=>{
+//   cy.get('#customCSS').click().clear().type(CustomCSS);
+//   cy.get('#customCSS').should('have.value',CustomCSS);
+//  })
+And ("User Click the button Save",()=>{
   cy.get('#button-submit').click();
+  cy.get('#button-submit').should('be.visible');
  })
- 
+ When ("User Click the button Edit",()=>{
+  cy.get(':nth-child(1) > [align="center"] > :nth-child(2)').click();
+ })
+ When("User edit Category", ()=>{
+  cy.get('#categoryId > .ant-select-selector > .ant-select-selection-item').click();
+  cy.get('[title="Longform"] > .ant-select-item-option-content').click();
+ })
+ When("User edit Title {}", (Title)=>{
+  cy.get('#translations-vn-title').click().clear().type(Title);
+  cy.get('#translations-vn-title').should('have.value', Title);
+ })
+  When("User edit Author {}", (Author)=>{
+  cy.get('#translations-vn-author').click().clear().type(Author);
+  cy.get('#translations-vn-author').should('have.value',Author);
+  })
+  When("User edit Introduce {}", (Introduce)=>{
+  cy.get('#translations-vn-summary').click().clear().type(Introduce);
+  cy.get('#translations-vn-summary').should('have.value',Introduce);
+  })
+  When("User edit Content {}", (Content)=>{
+  cy.get('.codex-editor__redactor').click().clear().type(Content);
+ })
